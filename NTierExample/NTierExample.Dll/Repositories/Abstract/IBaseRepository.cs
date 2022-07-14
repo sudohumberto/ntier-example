b1bbafcf648d46ccb6b13eb8f9b9ee7c9f0cbf58
Humberto Barrantes <sudohumberto@gmail.com>
2022-07-13 18:52:29 -0600

    Base Repository

---
+++ NTierExample/NTierExample.Dll/Repositories/Abstract/IBaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierExample.Dll.Repositories.Abstract
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        // SELECT methods
+        Task<T?> SelectById(int id);
+        Task<List<T>> SelectAll();
+
+        // INSERT methods
+        Task<T> Insert(T entity);
+
+        // UPDATE methods
+        void Update(T entity);
+
+        // DELETE methods
+        void Delete(int id);
+
+        // Save Changes methods
+        void SaveChanges();
+    }
+}
---
+++ NTierExample/NTierExample.Dll/Repositories/Implementation/BaseRepository.cs
@@ -0,0 +1,57 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using NTierExample.Dll.Repositories.Abstract;
+
+namespace NTierExample.Dll.Repositories.Implementation
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly DbContext _context = null!;
+        protected readonly DbSet<T> dbSet = null!;
+
+        protected BaseRepository(DbContext context)
+        {
+            _context = context;
+            dbSet = context.Set<T>();
+        }
+
+        public virtual async Task<T?> SelectById(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<List<T>> SelectAll()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public virtual async Task<T> Insert(T entity)
+        {
+            var addedEntity = (await dbSet.AddAsync(entity)).Entity;
+            await _context.SaveChangesAsync();
+            return addedEntity;
+        }
+
+        public virtual async void Update(T entity)
+        {
+            dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async void Delete(int id)
+        {
+            T? entity = dbSet.Find(id);
+            if (entity != null)
+            {
+                _ = dbSet.Remove(entity).Entity;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
